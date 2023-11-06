438fdc3831b153e2f9cd02535d784bbddefdba53
Jon William Taylor <jwtaylor659@gmail.com>
2023-11-06 12:04:40 -0600

    New repo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Image Classifier App</h1>
+    </header>
+    <main>
+      <SimpleImageClassifier />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleImageClassifier from './components/SimpleImageClassifier.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SimpleImageClassifier,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SimpleImageClassifier.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="file" accept="image/*" @change="classifyImage" />
+    <img v-if="imageUrl" :src="imageUrl" alt="Uploaded Image" style="max-width: 300px;" />
+    <p v-if="result">{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: null,
+      result: null,
+    };
+  },
+  methods: {
+    classifyImage(event) {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = (e) => {
+        this.imageUrl = e.target.result;
+      };
+      fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const labels = data.labels;
+        const prediction = Math.random() < 0.5 ? "cat" : "dog"
+        this.result = labels[prediction]
+      });
+    },
+  },
+};
+</script>
